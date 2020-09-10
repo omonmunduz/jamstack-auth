d4bfec6e0abcbe4b9a8734f8fa194187a2a052aa
omonmunduz <omurbekn@gmail.com>
2020-09-09 21:08:07 -0700

    feat: styles

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import {Link} from 'gatsby';
 import { css } from '@emotion/core';
+import { graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 import styled from '@emotion/styled';
 
-const HeroImage = styled("div")`
+const ImageBackground = styled(BackgroundImage)`
     height: 50vh;
-    background-image: url('/harley-davidson.jpg');
     background-position: bottom 30% center;
     background-size: cover;
+    padding: 5rem;
         + * {
             margin-top: 0;
         }
+
+    @media (max-width: 650px) {
+        padding: 1.2rem
+
+        }
 `;
 const TextBox = styled("div")`
+    background-color: #F7F7FF;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    padding: 2rem;
+    padding:  1.70rem;
+    width: 30%;
+
+    @media (max-width: 650px) {
+        width: 90%;
+        margin: 1.50rem auto;
+        }
 `;
 
 const Hero = () => {
+    const { image } = useStaticQuery(graphql`
+        query {
+            image: file(relativePath: {eq: "solen-feyissa.jpg"}){
+                sharp: childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+    `);
     return(
-        <HeroImage>
+        <ImageBackground Tag = "section" fluid = {image.sharp.fluid} fadeIn = "soft">
             <TextBox>
                 <h1> <span >&#x1D541;</span><span>&#x1D538;</span><span>&#x1D544;</span>stack is awesome</h1>
                 <p css = {css` text-transform: uppercase;`}>Log in to find out why</p>
@@ -37,7 +62,7 @@ const Hero = () => {
                         Go to the dashboard &rarr;
                     </Link>
             </TextBox>
-        </HeroImage>
+        </ImageBackground>
 
     )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import {css} from '@emotion/core';
-import {styled} from '@emotion/styled';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
 
